602deffd8db23da6bc1ebcf037b22d67e338fb44
MarcBarnaba <45950009+MarcBarnaba@users.noreply.github.com>
2022-02-24 23:28:30 +0100

    New models added
    Ho aggiunto le conf1 ai file dei modelli già esistenti, in modo da coprire tutte le configurazioni affrontate fino ad ora.
    Ho anche aggiornato la documentazione nel README, rendendola a mio avviso più chiara per l'utilizzatore.

--- train_model3_conf_att.py
+++ train_model3_conf_att.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import os
 from utilities.utils import read_ratings,read_graph_embeddings,read_bert_embedding,matching_Bert_Graph_conf
-from models.model3_conf2_att import run_model
+from models.model3_conf_att import run_model1,run_model2
 
 graph_embeddings = read_graph_embeddings("embeddings/DISTMULTembedding_768.json")
 user_bert_embeddings = read_bert_embedding("embeddings/UserProfiles_lastLayer.json")
@@ -13,7 +13,7 @@ item_bert_embeddings = read_bert_embedding("embeddings/ITEM_embeddingslastlayer.
 
 user, item, rating = read_ratings('datasets/dbbook/train2id.tsv')
 X, y, dim_embeddings = matching_Bert_Graph_conf(user,item,rating,graph_embeddings,user_bert_embeddings,item_bert_embeddings)
-model = run_model(X,y,dim_embeddings,epochs=30,batch_size=512)
+model = run_model1(X,y,dim_embeddings,epochs=30,batch_size=512)
 
 # creates a HDF5 file 'model.h5'
 model.save('results/model.h5')
--- train_model3_conf_strategy_att.py
+++ train_model3_conf_strategy_att.py
@@ -5,19 +5,16 @@ from keras.models import Sequential
 from keras.layers import Dense
 import os
 from utilities.utils import read_ratings,read_graph_embeddings,read_bert_embedding,matching_Bert_Graph
-from models.model2_conf2_strategy import run_model
+from models.model3_conf_strategy_att import run_model1,run_model2
 
-graph_embeddings = read_graph_embeddings("embeddings/HolEembedding_768.json")
+graph_embeddings = read_graph_embeddings("embeddings/TRANSDembedding_768.json")
 user_bert_embeddings = read_bert_embedding("embeddings/elmo_user_embeddings_nostopw_1024.json")
 item_bert_embeddings = read_bert_embedding("embeddings/elmo_embeddings_nostopw_1024.json")
 
 user, item, rating = read_ratings('datasets/movielens/train2id.tsv')
 X_graph,X_bert,dim_graph,dim_bert,y = matching_Bert_Graph(user,item,rating,graph_embeddings,user_bert_embeddings,item_bert_embeddings)
-model = run_model(X_graph,X_bert,dim_graph,dim_bert,y,epochs=25,batch_size=1536)
+model = run_model1(X_graph,X_bert,dim_graph,dim_bert,y,epochs=25,batch_size=1536)
 
 
 # creates a HDF5 file 'model.h5'
 model.save('results/model.h5')
-
-
-
